09d8cbca963122b9d528f7fca942aa7279647aa5
TranLeOfficial <tranthanhnhank123@gmail.com>
2020-08-10 11:13:34 +0700

    first commit

---
+++ QuanLyPhongTro/PhongModel.swift
@@ -0,0 +1,23 @@
+//
+//  PhongModel.swift
+//  QuanLyPhongTro
+//
+//  Created by Tran Thanh  Nhan on 8/6/20.
+//  Copyright © 2020 Tran Thanh  Nhan. All rights reserved.
+//
+
+import UIKit
+
+class PhongModel: NSObject {
+    
+    var id: Int?
+    var tenphong: String?
+    var trangthai: Int?
+    var songuoi: Int?
+    var ngaydat: String?
+    var tienphong: Int?
+    var ngaythanhtoan: String?
+    var id_nguoitao: Int?
+    var loaiphong: Int?
+    
+}
